a3685e0f92fc04795a7838cb8447a55de1437835
Lorenzo Natali <offtherailz@users.noreply.github.com>
2019-10-09 15:28:26 +0200

    moved everything in the docs dir

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'MapStore - Georchestra'
-copyright = '2019, Georchestra'
-author = 'Georchestra'
+project = 'MapStore Georchestra'
+copyright = '2019, GeoSolutions S.a.s.'
+author = 'GeoSolutions S.a.s.'
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. MapStore Georchestra documentation master file, created by
+   sphinx-quickstart on Wed Oct  9 15:27:23 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MapStore Georchestra's documentation!
+================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
