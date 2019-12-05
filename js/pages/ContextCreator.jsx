b117f95775a0adcf1d2f69c87925f5c678acb411
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2019-12-05 12:29:38 +0100

    reverted to previous solution for map layout, removed unneeded configs

--- js/pages/ContextCreator.jsx
+++ js/pages/ContextCreator.jsx
@@ -1,13 +1,16 @@
-import React, {useEffect} from "react";
+import React /* {useEffect} */ from "react";
 import Page from '@mapstore/product/pages/ContextCreator';
-import ConfigUtils from "@mapstore/utils/ConfigUtils";
+// import ConfigUtils from "@mapstore/utils/ConfigUtils";
 
 export default (props) => {
+    /* This scauses a wrong effect with the size of the TOC, Catalog and other side panels
+    // TODO: fix issues with layout and restore FeatureGrid, and remove css fixes from georchestra.less
     useEffect(() => {
         ConfigUtils.setConfigProp('mapLayout', { left: { sm: 300, md: 500, lg: 600 }, right: { md: 658 }, bottom: { sm: 130 } });
         return () => {
             ConfigUtils.setConfigProp('mapLayout', { left: { sm: 300, md: 500, lg: 600 }, right: { md: 658 }, bottom: { sm: 30 } });
         };
     }, []);
+    */
     return <Page {...props}/>;
 };
