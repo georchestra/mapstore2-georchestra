d61776d93d21278d3b752ca4b0150235323b139f
Suren <dsuren1@gmail.com>
2021-11-26 21:18:12 +0530

    #401: Versioning repository and submodule (#430)

---
+++ revision.js
@@ -0,0 +1,20 @@
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin');
+const DefinePlugin = require("webpack/lib/DefinePlugin");
+
+const parseCommand = () => {
+    const commands = [
+        'rev-parse HEAD', // Repository commit hash
+        'rev-parse --abbrev-ref HEAD', // Repository branch name
+        'remote get-url origin', // Repository url
+        'submodule status', // Submodule info
+        'submodule foreach -q git config remote.origin.url' // Submodule url
+    ]
+    return commands.map(command => JSON.stringify(
+        new GitRevisionPlugin({
+            branchCommand: command,
+        }).branch()
+    ));
+};
+module.exports = new DefinePlugin({
+    '__VERSIONINFO__': parseCommand()
+});
