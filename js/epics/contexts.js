9efdc846fbd7c9c69330d4ed5f5e9bb21002324e
Alexander <mineralex1991@gmail.com>
2022-01-12 22:35:54 +0300

    Update configuration with changes ported back to the Mapstore upstream

--- js/epics/contexts.js
+++
@@ -1,7 +0,0 @@
-import {MAPS_LIST_LOADING} from "@mapstore/actions/maps";
-import Rx from "rxjs";
-import {searchContexts} from "@mapstore/actions/contextmanager";
-
-export const searchContextsOnMapSearch = action$ =>
-    action$.ofType(MAPS_LIST_LOADING)
-        .switchMap(({ searchText }) => Rx.Observable.of(searchContexts(searchText)));
