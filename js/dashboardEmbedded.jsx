dedaa0f80d10d4622c69b5a67466333077ad5b06
Alexander <alexander.fedorenko@geosolutionsgroup.com>
2022-02-10 13:26:03 +0300

    Proper configuration for embedded geostories and dashboards. (#491)
    (cherry picked from commit 4d1f07c78f688af87f8fce485f14913c81dfc62b)

---
+++ js/dashboardEmbedded.jsx
@@ -0,0 +1,26 @@
+import {
+    setConfigProp,
+    setLocalConfigurationFile
+} from '@mapstore/utils/ConfigUtils';
+
+// Add custom (overriding) translations
+// example for additional translations in the project folder
+// setConfigProp('translationsPath', ['./MapStore2/web/client/translations', './translations']);
+setConfigProp("translationsPath", [
+    "./MapStore2/web/client/translations",
+    "./translations"
+]);
+setConfigProp("geoStoreUrl", "rest/geostore/");
+setConfigProp("configurationFolder", "rest/config/load/");
+
+
+// __PROJECTNAME__ is the name of the project used in the creation process
+setConfigProp('themePrefix', 'geOrchestra');
+
+// Use a custom plugins configuration file
+// example if localConfig.json is located in the root of the project
+// setLocalConfigurationFile('localConfig.json');
+setLocalConfigurationFile("configs/localConfig.json");
+
+// async load of the standard embedded bundle
+import('@mapstore/product/dashboardEmbedded');
