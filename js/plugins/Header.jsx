a233d0bca953298647dcab37d3b9c686d41225d0
Florian Necas <florian.necas@camptocamp.com>
2023-11-10 16:13:14 +0100

    feat: implement new header
    https://github.com/georchestra/georchestra/pull/4065

--- js/plugins/Header.jsx
+++ js/plugins/Header.jsx
@@ -8,21 +8,39 @@
 import {useEffect} from "react";
 import { createPlugin, connect } from "@mapstore/utils/PluginsUtils";
 
-export const Header = ({url = "/header/", page = "mapstore", height = 90, ignoreIFrame = false}) => {
+export const Header = ({url = "/header/", page = "mapstore", height = 90, ignoreIFrame = false, script = "https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js", legacy = false}) => {
     useEffect(() => {
         const header = document.getElementById("georchestra-header");
         const container = document.getElementById("container");
         if (header) {
+            // Prevent creation of multiple headers
+            const existingGeorHeader = document.getElementsByTagName("geor-header");
+            if (existingGeorHeader?.length > 0) {
+                existingGeorHeader[0].remove();
+            }
+            const existingScript = document.querySelectorAll("script[src='" + script + "']");
+            if (existingScript?.length > 0) {
+                existingScript[0].remove();
+            }
             if (!ignoreIFrame && window.location !== window.parent.location) {
                 header.style.display = 'none';
                 if (container) {
                     container.style.top = '0';
                 }
             } else {
-                header.style.display = 'block';
-                header.src = url + "?active=" + page;
+                const georHeader = document.createElement("geor-header");
+                const georHeaderScript = document.createElement("script");
+
+                georHeader.setAttribute("active-app", page);
+                georHeader.setAttribute("legacy-header", false);
+                georHeader.setAttribute("legacy-url", url);
+                georHeader.setAttribute("legacy-header", legacy);
+                georHeaderScript.src = script;
                 header.style.height = height + "px";
 
+
+                header.appendChild(georHeader);
+                header.appendChild(georHeaderScript);
                 if (container) {
                     container.style.top = height + "px";
                 }
@@ -36,6 +54,8 @@ export const Header = ({url = "/header/", page = "mapstore", height = 90, ignore
 export default createPlugin('Header', {
     component: connect((state) => ({
         url: state.localConfig && state.localConfig.header && state.localConfig.header.url,
-        height: state.localConfig && state.localConfig.header && state.localConfig.header.height
+        height: state.localConfig && state.localConfig.header && state.localConfig.header.height,
+        script: state.localConfig && state.localConfig.header && state.localConfig.header.script,
+        legacy: state.localConfig && state.localConfig.header && state.localConfig.header.legacy
     }))(Header)
 });
