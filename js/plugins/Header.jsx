756d70393d985f43cc0bb0030796f8a807295129
Florian Necas <florian.necas@camptocamp.com>
2025-01-08 16:43:19 +0100

    feat: set header to config file
    (cherry picked from commit 5dc35d31cbba1af4a4371c3921c0a6b76c608be9)

--- js/plugins/Header.jsx
+++ js/plugins/Header.jsx
@@ -8,30 +8,47 @@
 import {useEffect} from "react";
 import { createPlugin, connect } from "@mapstore/utils/PluginsUtils";
 
-export const Header = ({ page = "mapstore", height = 80,
+export const Header = ({url = "/header/", page = "mapstore", height = 90, ignoreIFrame = false,
     script = "https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js",
+    legacy = false,
+    logoUrl = "https://www.georchestra.org/public/georchestra-logo.svg",
     stylesheet = "", configFile = ""}) => {
     useEffect(() => {
         const header = document.getElementById("georchestra-header");
         const headerScript = document.getElementById("georchestra-header-script");
         const container = document.getElementById("container");
         if (header) {
-            header.setAttribute("active-app", page);
-            header.setAttribute("stylesheet", stylesheet);
-            header.setAttribute("config-file", configFile);
-            headerScript.src = script;
-            if (container) {
-                container.style.top = height + "px";
+            if (!ignoreIFrame && window.location !== window.parent.location) {
+                header.style.display = 'none';
+                if (container) {
+                    container.style.top = '0';
+                }
+            } else {
+                header.setAttribute("active-app", page);
+                header.setAttribute("legacy-url", url);
+                header.setAttribute("legacy-header", legacy);
+                header.setAttribute("logo-url", logoUrl);
+                header.setAttribute("stylesheet", stylesheet);
+                header.setAttribute("config-file", configFile);
+                headerScript.src = script;
+
+                if (container) {
+                    container.style.top = height + "px";
+                }
             }
+
         }
-    }, [page, window.location, window.parent.location]);
+    }, [page, url, height, window.location, window.parent.location]);
     return null;
 };
 
 export default createPlugin('Header', {
     component: connect((state) => ({
-        script: state.localConfig && state.localConfig.header && state.localConfig.header.script,
+        url: state.localConfig && state.localConfig.header && state.localConfig.header.url,
         height: state.localConfig && state.localConfig.header && state.localConfig.header.height,
+        script: state.localConfig && state.localConfig.header && state.localConfig.header.script,
+        legacy: state.localConfig && state.localConfig.header && state.localConfig.header.legacy,
+        logoUrl: state.localConfig && state.localConfig.header && state.localConfig.header.logoUrl,
         stylesheet: state.localConfig && state.localConfig.header && state.localConfig.header.stylesheet,
         configFile: state.localConfig && state.localConfig.header && state.localConfig.header.configFile
     }))(Header)
